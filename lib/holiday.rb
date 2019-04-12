a92aa631acdbaef7ea6f088a4b22af54c5911a7a
Preston Elliott <preston.elliott24@gmail.com>
2019-04-12 18:57:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
-      if supplies == "BBQ"
-        "#{holiday}"
+      if supplies = "BBQ"
+        puts "#{holiday}"
       end
     end
   end
