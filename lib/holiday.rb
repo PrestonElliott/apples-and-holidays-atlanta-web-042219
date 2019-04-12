9b6702f5d3a83dbd890cdf699fd3161cdf74ea0c
Preston Elliott <preston.elliott24@gmail.com>
2019-04-12 19:01:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,6 @@ def all_holidays_with_bbq(holiday_hash)
       holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
+end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
-end
