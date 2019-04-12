e4ebcaf32a179a31849af9cd80876bb195fd96ff
Preston Elliott <preston.elliott24@gmail.com>
2019-04-12 18:11:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,9 +33,7 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].collect do |holiday, supplies|
-    supplies << "Grill"
-    supplies.pop
-    supplies << "Table Cloth"
+    supplies << supply
   end
   # holiday_hash[:spring][:memorial_day] "Grill" = "Table Cloth"
   # holiday_hash[:spring][:memorial_day].pop
